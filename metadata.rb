3468ed524e286ada00d9b51e116149f648e3a9f3
Vinícius Kirst <vinicius@versul.com.br>
2016-08-08 17:02:08 -0300

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'ruby2'
+maintainer       'Vinícius Kirst'
+maintainer_email 'vinicius.kirst@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures ruby2'
+long_description 'Installs/Configures ruby2'
+version          '0.1.0'
+
+depends 'apt'
